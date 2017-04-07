fc08a91fac25be7df5463972e02ded908d176d99
Ankit chaturvedi <erankit4uu@gmail.com>
2017-04-07 15:50:43 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/ankitchaturvedi/girddemo/Utils/Constant.java
@@ -0,0 +1,11 @@
+package com.example.ankitchaturvedi.girddemo.Utils;
+
+/**
+ * Created by Ankit.Chaturvedi on 01-Apr-17.
+ */
+
+public class Constant {
+
+    public static final String TXT_BLANK = "";
+    public static String api_key = "Your Api Key";
+}
